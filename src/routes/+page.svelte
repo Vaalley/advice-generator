3d7841eb35d9e1cb4d442cf1e45fd28b213672b1
Vaalley <vaaleyou@gmail.com>
2023-03-02 02:12:13 +0100

    almost done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,68 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let advices = [
+		{
+			id: 1,
+			advice: 'A long walk alone with some time to think, can work wonders.'
+		},
+		{
+			id: 2,
+			advice: 'Test2'
+		},
+		{
+			id: 3,
+			advice: 'Test3'
+		},
+		{
+			id: 4,
+			advice: 'Test4'
+		}
+	];
+
+	function getAdvice() {
+		let rng = Math.floor(Math.random() * advices.length);
+		let adviceId = document.getElementsByClassName('advice-id')[0];
+		let adviceAdvice = document.getElementsByClassName('advice-advice')[0];
+
+		adviceId.innerHTML = `ADVICE #`.concat(advices[rng].id);
+		adviceAdvice.innerHTML = `“`.concat(advices[rng].advice).concat(`”`);
+	}
+
+	onMount(() => {
+		getAdvice();
+
+		window.addEventListener('keydown', (e) => {
+			if (e.key === ' ') {
+				getAdvice();
+			}
+		});
+	});
+</script>
+
 <svelte:head>
-	<title>A Basic Title</title>
-	<link rel="icon" href="" />
+	<title>Advice generator</title>
+	<link rel="icon" href="/images/favicon-32x32.png" />
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="w-screen h-screen flex justify-center items-center bg-dark-blue font-roboto">
+	<div
+		class="w-[600px] h-fit flex flex-col justify-end items-center bg-dark-grayish-blue shadow-2xl rounded-2xl px-4"
+	>
+		<p class="advice-id text-neon-green text-sm tracking-widest font-bold text-center p-10" />
+
+		<p class="advice-advice text-light-cyan text-3xl font-semibold text-center mb-auto" />
+
+		<img src="images/pattern-divider-desktop.svg" class="mt-10 mb-4" alt="Divider desktop" />
+
+		<button on:click={getAdvice} class="p-5 bg-neon-green rounded-full w-fit "
+			><img src="/images/icon-dice.svg" alt="Dice icon for randon advice" /></button
+		>
+	</div>
+</main>
+
+<style>
+	button {
+		transform: translate(0%, 50%);
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      screens: {
-        // '-2xl': { max: '1439px' },
-        '-xl': { max: '1279px' },
-        '-lg': { max: '1023px' },
-        '-md': { max: '767px' },
-        '-sm': { max: '639px' },
-      },
-      colors: {
-      },
-      fontFamily: {
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			screens: {
+				// '-2xl': { max: '1439px' },
+				'-xl': { max: '1279px' },
+				'-lg': { max: '1023px' },
+				'-md': { max: '767px' },
+				'-sm': { max: '639px' }
+			},
+			colors: {
+				'light-cyan': 'hsl(193, 38%, 86%)',
+				'neon-green': 'hsl(150, 100%, 66%)',
+				'grayish-blue': 'hsl(217, 19%, 38%)',
+				'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+				'dark-blue': 'hsl(218, 23%, 16%)'
+			},
+			fontFamily: {
+				Roboto: ['Roboto', 'sans-serif'],
+				'dancing-script': ['Dancing Script', 'cursive']
+			}
+		}
+	},
+	plugins: []
+};
